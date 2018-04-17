081a2212b883b335265413fe4f84343ffd0e428c
Interchained <president@worldvaporexpo.com>
2018-04-16 21:13:46 -0400

    Update cryptonote_config.h

--- src/cryptonote_config.h
+++ src/cryptonote_config.h
@@ -150,6 +150,7 @@
 // New constants are intended to go here
 namespace config
 {
+  uint64_t const SEGREGATION_FORK_HEIGHT 241501
   uint64_t const DEFAULT_FEE_ATOMIC_XMR_PER_KB = 500; // Just a placeholder!  Change me!
   uint8_t const FEE_CALCULATION_MAX_RETRIES = 10;
   uint64_t const DEFAULT_DUST_THRESHOLD = ((uint64_t)2); // 2 * pow(10, 1)
@@ -170,6 +171,7 @@ namespace config
 
   namespace testnet
   {
+    uint64_t const TESTNET_SEGREGATION_FORK_HEIGHT 1000000
     uint64_t const CRYPTONOTE_PUBLIC_ADDRESS_BASE58_PREFIX = 53;
     uint64_t const CRYPTONOTE_PUBLIC_INTEGRATED_ADDRESS_BASE58_PREFIX = 54;
     uint64_t const CRYPTONOTE_PUBLIC_SUBADDRESS_BASE58_PREFIX = 63;
@@ -185,6 +187,7 @@ namespace config
 
   namespace stagenet
   {
+    uint64_t const STAGENET_SEGREGATION_FORK_HEIGHT 1000000
     uint64_t const CRYPTONOTE_PUBLIC_ADDRESS_BASE58_PREFIX = 24;
     uint64_t const CRYPTONOTE_PUBLIC_INTEGRATED_ADDRESS_BASE58_PREFIX = 25;
     uint64_t const CRYPTONOTE_PUBLIC_SUBADDRESS_BASE58_PREFIX = 36;
