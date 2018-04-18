dcccf006be1df5683237bc390e77455b1b1da373
Interchained <president@worldvaporexpo.com>
2018-04-17 23:00:04 -0400

    Update cryptonote_config.h

--- src/cryptonote_config.h
+++ src/cryptonote_config.h
@@ -158,13 +158,13 @@ namespace config
   uint64_t const ELECTRONERO_HARDFORK_HEIGHT = ((uint64_t)(241525)); // initial fork height
   
   uint64_t const MAINNET_HARDFORK_V1_HEIGHT = ((uint64_t)(1)); // v1
-  uint64_t const MAINNET_HARDFORK_V2_HEIGHT = ((uint64_t)(241525)); // v2
-  uint64_t const MAINNET_HARDFORK_V3_HEIGHT = ((uint64_t)(239950)); // v3
-  uint64_t const MAINNET_HARDFORK_V4_HEIGHT = ((uint64_t)(239960)); // v4
-  uint64_t const MAINNET_HARDFORK_V5_HEIGHT = ((uint64_t)(239970)); // v5
+  uint64_t const MAINNET_HARDFORK_V2_HEIGHT = ((uint64_t)(240001)); // v2
+  uint64_t const MAINNET_HARDFORK_V3_HEIGHT = ((uint64_t)(239965)); // v3
+  uint64_t const MAINNET_HARDFORK_V4_HEIGHT = ((uint64_t)(239975)); // v4
+  uint64_t const MAINNET_HARDFORK_V5_HEIGHT = ((uint64_t)(239985)); // v5
   uint64_t const MAINNET_HARDFORK_V6_HEIGHT = ((uint64_t)(239930)); // v6
   uint64_t const MAINNET_HARDFORK_V7_HEIGHT = ((uint64_t)(239949)); // v7
-  uint64_t const MAINNET_HARDFORK_V8_HEIGHT = ((uint64_t)(242000)); // v8
+  uint64_t const MAINNET_HARDFORK_V8_HEIGHT = ((uint64_t)(239960)); // v8
   
   uint64_t const DEFAULT_FEE_ATOMIC_XMR_PER_KB = 500; // Just a placeholder!  Change me!
   uint8_t const FEE_CALCULATION_MAX_RETRIES = 10;
