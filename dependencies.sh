2dc3e7ecbeecca3861d03521c7762e93e67bf05b
Interchained <president@worldvaporexpo.com>
2018-04-18 19:18:20 -0400

    Delete dependencies.sh

--- dependencies.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-apt-get install build-essential cmake pkg-config libsodium-dev libboost-all-dev libssl-dev libzmq3-dev libunbound-dev libminiupnpc-dev libunwind8-dev liblzma-dev libreadline6-dev libldns-dev libexpat1-dev libgtest-dev doxygen graphviz
