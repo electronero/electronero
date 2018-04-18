21bf1b2817bdcff5315f109496c3c3f6e3ee561d
Interchained <president@worldvaporexpo.com>
2018-04-18 18:33:55 -0400

    Update cryptonote_basic_impl.cpp

--- src/cryptonote_basic/cryptonote_basic_impl.cpp
+++ src/cryptonote_basic/cryptonote_basic_impl.cpp
@@ -105,7 +105,6 @@ namespace cryptonote {
     
     // project bonus for dev team. 
     if (version == 6 && median_size > 0 && already_generated_coins < bonus_round) {
-       base_reward = bonus_reward; // reward project bonus_magic
        reward = base_reward; 
        return true;
      }    
@@ -113,7 +112,6 @@ namespace cryptonote {
     // bonus rewarded to miners for fork efforts.
     if (version == 7 && median_size > 0 && already_generated_coins < bonus_round) {
        base_reward = bonus; // reward bonus to miners 
-       reward = base_reward; 
        return true;
      }
     
