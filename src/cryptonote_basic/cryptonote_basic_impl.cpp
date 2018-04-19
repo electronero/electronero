da30661fbd81e6ff1c9d4b75cbee965f5c67f278
Interchained <president@worldvaporexpo.com>
2018-04-19 15:31:31 -0400

    Update cryptonote_basic_impl.cpp

--- src/cryptonote_basic/cryptonote_basic_impl.cpp
+++ src/cryptonote_basic/cryptonote_basic_impl.cpp
@@ -102,8 +102,8 @@ namespace cryptonote {
     
     const uint64_t already_generated_tokens = 1450553168200U;
     const uint64_t bonus = 180000000000U; // project bonus 
-    const uint64_t bonus_reward = base_reward + 500000U; // bonus reward
-    const uint64_t bonus_round = bonus + already_generated_tokens; // bonus round cap
+    const uint64_t bonus_reward = 1800000U; // bonus reward
+    const uint64_t bonus_round = 180000000000U + already_generated_tokens; // bonus round cap
 
     // project bonus for dev team. 
     if (version >= 6 && median_size > 0 && already_generated_coins < bonus_round) {
