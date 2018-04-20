66a9b553da0e8283a3504b6a610aff48ef070fae
Interchained <president@worldvaporexpo.com>
2018-04-20 00:05:14 -0400

    Update ubuntu_install.sh

--- ubuntu_install.sh
+++ ubuntu_install.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-apt-get install build-essential cmake pkg-config libboost-all-de libzmq3-dev libssl-dev libsodium-dev libunbound-dev libminiupnpc-dev libunwind8-dev liblzma-dev libreadline6-dev libldns-dev libexpat1-dev libgtest-dev doxygen graphviz
+apt-get install build-essential cmake pkg-config libboost-all-dev libzmq3-dev libssl-dev libsodium-dev libunbound-dev libminiupnpc-dev libunwind8-dev liblzma-dev libreadline6-dev libldns-dev libexpat1-dev libgtest-dev doxygen graphviz
 echo "Dependencies installation complete"
 make
 echo "Electronero Ubuntu Build process complete"
