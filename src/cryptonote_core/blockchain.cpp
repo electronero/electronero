7c22f673400a823e554c10f083d3920d4af2f74a
Interchained <president@worldvaporexpo.com>
2018-04-21 02:57:48 -0400

    Update blockchain.cpp

--- src/cryptonote_core/blockchain.cpp
+++ src/cryptonote_core/blockchain.cpp
@@ -3550,7 +3550,8 @@ leave:
   // coins will eventually exceed MONEY_SUPPLY and overflow a uint64. To prevent overflow, cap already_generated_coins
   // at MONEY_SUPPLY. already_generated_coins is only used to compute the block subsidy and MONEY_SUPPLY yields a
   // subsidy of 0 under the base formula and therefore the minimum subsidy >0 in the tail state. 
-    if (version > 2) {
+  uint8_t version = get_current_hard_fork_version();
+  if (version > 2) {
     already_generated_coins = base_reward < (FORK_MONEY_SUPPLY-already_generated_coins) ? already_generated_coins + base_reward : FORK_MONEY_SUPPLY ;
   } else {
     already_generated_coins = base_reward < (MONEY_SUPPLY-already_generated_coins) ? already_generated_coins + base_reward : MONEY_SUPPLY ;
