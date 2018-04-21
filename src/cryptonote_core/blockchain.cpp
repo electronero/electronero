755fd968c575558e3dca4c8cabad3d483c3572b7
Interchained <president@worldvaporexpo.com>
2018-04-21 03:22:21 -0400

    Update blockchain.cpp

--- src/cryptonote_core/blockchain.cpp
+++ src/cryptonote_core/blockchain.cpp
@@ -3551,11 +3551,12 @@ leave:
   // at MONEY_SUPPLY. already_generated_coins is only used to compute the block subsidy and MONEY_SUPPLY yields a
   // subsidy of 0 under the base formula and therefore the minimum subsidy >0 in the tail state. 
   uint8_t version = get_current_hard_fork_version();
-  if (version > 2) {
-    already_generated_coins = base_reward < (FORK_MONEY_SUPPLY-already_generated_coins) ? already_generated_coins + base_reward : FORK_MONEY_SUPPLY ;
-  } else {
+  if (version == 1) {
     already_generated_coins = base_reward < (MONEY_SUPPLY-already_generated_coins) ? already_generated_coins + base_reward : MONEY_SUPPLY ;
   }
+  if (version > 2) {
+    already_generated_coins = base_reward < (FORK_MONEY_SUPPLY-already_generated_coins) ? already_generated_coins + base_reward : FORK_MONEY_SUPPLY ;
+  } 
   if(m_db->height())
     cumulative_difficulty += m_db->get_block_cumulative_difficulty(m_db->height() - 1);
 
