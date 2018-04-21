37e9b7ca01bc7d88a09e507bbb4d134839295b52
Mark Allen Evans <president@worldvaporexpo.com>
2018-04-21 08:18:03 +0000

    cleanup

---
+++ retar.sh
@@ -0,0 +1,7 @@
+cd build/release/bin
+tar -cvzf electronero_ubuntu64.tar.gz  electronero-blockchain-import electronero-blockchain-export electronerod electronero-gen-trusted-multisig electronero-wallet-cli electronero-wallet-rpc electronero-blockchain-blackball electronero-blockchain-usage
+rm -rf /var/www/html/website/dl/electronero_ubuntu64.tar.gz 
+cp electronero_ubuntu64.tar.gz /var/www/html/website/dl/ -r -f 
+rm -rf ../../../electronero_ubuntu64.tar.gz 
+mv electronero_ubuntu64.tar.gz ../../../
+./electronerod
