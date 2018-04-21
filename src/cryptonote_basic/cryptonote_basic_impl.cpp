64da618808218f46297cc4f24f61b5b13c3fc745
Interchained <president@worldvaporexpo.com>
2018-04-21 02:18:03 -0400

    Update cryptonote_basic_impl.cpp

--- src/cryptonote_basic/cryptonote_basic_impl.cpp
+++ src/cryptonote_basic/cryptonote_basic_impl.cpp
@@ -89,9 +89,10 @@ namespace cryptonote {
   bool get_block_reward(size_t median_size, size_t current_block_size, uint64_t already_generated_coins, uint64_t &reward, uint8_t version, uint64_t height) {
     static_assert(DIFFICULTY_TARGET_V2%60==0&&DIFFICULTY_TARGET_V1%60==0,"difficulty targets must be a multiple of 60");
     const int target = version < 2 ? DIFFICULTY_TARGET_V1 : DIFFICULTY_TARGET_V2;
+    const int TOKEN_SUPPLY = version < 7 ? MONEY_SUPPLY : FORK_MONEY_SUPPLY;
     const int target_minutes = target / 60;
     const int emission_speed_factor = EMISSION_SPEED_FACTOR_PER_MINUTE - (target_minutes-1);
-
+    uint64_t base_reward = (TOKEN_SUPPLY - already_generated_coins) >> emission_speed_factor;
     const uint64_t premine = 1260000000000U;
     if (height == 1 && already_generated_coins < premine) {
       reward = premine;
@@ -101,23 +102,22 @@ namespace cryptonote {
       reward = premine;
       return true;
     }
-    uint64_t base_reward;
     uint64_t round_factor = 10000000; // 1 * pow(10, 7)
     if (version >= 7 && height > 239924)
     {
       if (height < (PEAK_COIN_EMISSION_HEIGHT + COIN_EMISSION_HEIGHT_INTERVAL)) {
         uint64_t interval_num = height / COIN_EMISSION_HEIGHT_INTERVAL;
         double money_supply_pct = 0.1888 + interval_num*(0.023 + interval_num*0.0032);
-        base_reward = ((uint64_t)(FORK_MONEY_SUPPLY * money_supply_pct)) >> emission_speed_factor;
+        base_reward = ((uint64_t)(TOKEN_SUPPLY * money_supply_pct)) >> emission_speed_factor;
       }
       else{
-        base_reward = (FORK_MONEY_SUPPLY - already_generated_coins) >> emission_speed_factor;
+        base_reward = (TOKEN_SUPPLY - already_generated_coins) >> emission_speed_factor;
       }
     }
     else
     {
       // do something
-      base_reward = (MONEY_SUPPLY - already_generated_coins) >> emission_speed_factor;
+      base_reward = (TOKEN_SUPPLY - already_generated_coins) >> emission_speed_factor;
     }
     
     const uint64_t FINITE_SUBSIDY = 100U;
