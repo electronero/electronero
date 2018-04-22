98ef2302dca20db12770dfd39789a89042356c87
Interchained <president@worldvaporexpo.com>
2018-04-21 21:06:38 -0400

    Update cryptonote_config.h

--- src/cryptonote_config.h
+++ src/cryptonote_config.h
@@ -67,14 +67,14 @@
 #define COIN                                            ((uint64_t)100) // pow(10, 2)
 
 #define CRYPTONOTE_TX_FEE_RESERVED_SIZE               3
-#define CRYPTONOTE_BLOCK_FEE_REWARD_ZONE_V5           9
+#define CRYPTONOTE_BLOCK_FEE_REWARD_ZONE_V5           21
 
 #define FEE_PER_KB_OLD                                  ((uint64_t)10) // .1 * pow(10, 1)
 #define FEE_PER_KB_V2                                   ((uint64_t)40) // .4 * pow(10, 1)
 #define FEE_PER_KB                                      ((uint64_t)10000) // 100 * pow(10, 2)
 #define DYNAMIC_FEE_PER_KB_BASE_FEE                     ((uint64_t)10) // .1 * pow(10, 1)
 #define DYNAMIC_FEE_PER_KB_BASE_BLOCK_REWARD            ((uint64_t)10) // .10 * pow(10, 1)
-#define DYNAMIC_FEE_PER_KB_BASE_FEE_V5                  ((uint64_t)1 * (uint64_t)CRYPTONOTE_TX_FEE_RESERVED_SIZE / CRYPTONOTE_BLOCK_FEE_REWARD_ZONE_V5)
+#define DYNAMIC_FEE_PER_KB_BASE_FEE_V5                  ((uint64_t)10 * (uint64_t)CRYPTONOTE_TX_FEE_RESERVED_SIZE / CRYPTONOTE_BLOCK_FEE_REWARD_ZONE_V5)
 
 #define ORPHANED_BLOCKS_MAX_COUNT                       100
 
