1f3dea7635d5f4deee0a04c327a6cb0d81ef7f76
Interchained <president@worldvaporexpo.com>
2018-05-18 01:20:04 -0400

    MIN_MIXIN

--- src/wallet/wallet_rpc_server.cpp
+++ src/wallet/wallet_rpc_server.cpp
@@ -785,9 +785,9 @@ namespace tools
     try
     {
       uint64_t mixin = req.ring_size;
-      if (mixin < DEFAULT_MIXIN){
-        LOG_PRINT_L1("Requested mixin " << req.ring_size << " too low, using " << DEFAULT_MIXIN << " if this doesnt seem right, perhaps we should be using " << req.mixin);
-        mixin = DEFAULT_MIXIN;
+      if (mixin < MIN_MIXIN){
+        LOG_PRINT_L1("Requested mixin " << req.ring_size << " too low, using " << MIN_MIXIN << " if this doesnt seem right, perhaps we should be using " << req.mixin);
+        mixin = MIN_MIXIN;
       }
       else if (mixin > MAX_MIXIN){
         LOG_PRINT_L1("Requested mixin " << req.ring_size << " too high, using " << MAX_MIXIN << " if this doesnt seem right, perhaps we should be using " << req.mixin);
