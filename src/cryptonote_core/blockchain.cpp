f0dc250b3e05c572b769f6e7b0d30f6244404d57
Interchained <president@worldvaporexpo.com>
2018-05-29 23:31:20 -0400

    Update blockchain.cpp

--- src/cryptonote_core/blockchain.cpp
+++ src/cryptonote_core/blockchain.cpp
@@ -64,6 +64,7 @@
 #define MAINNET_HARDFORK_V1_HEIGHT ((uint64_t)(1)) // MAINNET v1 
 #define MAINNET_HARDFORK_V7_HEIGHT ((uint64_t)(307003)) // MAINNET v7 hard fork 
 #define MAINNET_HARDFORK_V8_HEIGHT ((uint64_t)(307057)) // MAINNET v8 hard fork 
+#define MAINNET_HARDFORK_V9_HEIGHT ((uint64_t)(307249)) // MAINNET v8 hard fork 
 
 #define TESTNET_ELECTRONERO_HARDFORK ((uint64_t)(12746)) // Electronero TESTNET fork height
 #define TESTNET_HARDFORK_NETWORK ((uint64_t)(25653086)) // TESTNET cumulative difficulties 
@@ -117,11 +118,14 @@ static const struct {
   // version 6 starts from block 1400000, which is on or around the 16th of September, 2017. Fork time finalised on 2017-08-18.
   // { 6, MAINNET_HARDFORK_V6_HEIGHT, 0, 1524279224 },
 
-  // version 7 starts from block 1546000, which is on or around the 6th of April, 2018. Fork time finalised on 2018-03-17.
+  // version 7 starts from block 307003, which is on or around the 6th of April, 2018. Fork time finalised on 2018-03-17.
   { 7, MAINNET_HARDFORK_V7_HEIGHT, 0, 1527643352 },
 	
-  // version 7 starts from block 1546000, which is on or around the 6th of April, 2018. Fork time finalised on 2018-03-17.
+  // version 7 starts from block 307057, which is on or around the 6th of April, 2018. Fork time finalised on 2018-03-17.
   { 8, MAINNET_HARDFORK_V8_HEIGHT, 0, 1527646352 },
+  
+  // version 9 starts from block 307249, which is on or around the 5th of May, 2018. Fork time finalised on 2018-05-17.
+  { 9, MAINNET_HARDFORK_V9_HEIGHT, 0, 1527651007 },
 };
 static const uint64_t mainnet_hard_fork_version_1_till = MAINNET_HARDFORK_V7_HEIGHT-1;
 
