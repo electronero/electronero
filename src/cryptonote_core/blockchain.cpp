5a04a80caa7d3a44b314e471cea294d79eb3c81e
Interchained <president@worldvaporexpo.com>
2018-06-02 05:25:55 -0400

    Update blockchain.cpp

--- src/cryptonote_core/blockchain.cpp
+++ src/cryptonote_core/blockchain.cpp
@@ -3486,7 +3486,7 @@ leave:
     }
     else
       proof_of_work = get_block_longhash(bl, m_db->height());
-
+    const uint64_t bc_height = m_db->height() - 1;
     if (bc_height >= 309500) {   
     // validate proof_of_work versus difficulty target
     if(!check_hash(proof_of_work, current_diffic))
