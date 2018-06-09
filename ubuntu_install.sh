19e0ea3bd1f162a8eb2f62401a9f394037b0bbf7
Mark Allen Evans <president@worldvaporexpo.com>
2018-06-09 06:30:02 +0000

    rebase

--- ubuntu_install.sh
+++ ubuntu_install.sh
@@ -2,6 +2,4 @@
 apt-get install build-essential cmake pkg-config libboost-all-dev libzmq3-dev libssl-dev libsodium-dev libunbound-dev libminiupnpc-dev libunwind8-dev liblzma-dev libreadline6-dev libldns-dev libexpat1-dev libgtest-dev doxygen graphviz
 echo "Dependencies installation complete"
 echo "Download Submodules"
-git submodule init && git submodule update
-make
-echo "Electronero Ubuntu Build process complete"
+
