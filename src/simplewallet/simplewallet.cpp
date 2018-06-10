89ca2c2f103ab15751b8a598edd02fc234ddbfaa
Interchained <president@worldvaporexpo.com>
2018-06-09 22:56:38 -0400

    Update simplewallet.cpp

--- src/simplewallet/simplewallet.cpp
+++ src/simplewallet/simplewallet.cpp
@@ -727,11 +727,8 @@ bool simple_wallet::print_fee_info(const std::vector<std::string> &args/* = std:
   }
   const uint64_t per_kb_fee = m_wallet->get_per_kb_fee();
   const uint64_t typical_size_kb = 13;
-<<<<<<< HEAD
-  message_writer() << (boost::format(tr("Current fee is %s ETNX per kB")) % print_money(per_kb_fee)).str();
-=======
   message_writer() << (boost::format(tr("Current fee is %s %s per kB")) % print_money(per_kb_fee) % cryptonote::get_unit(cryptonote::get_default_decimal_point())).str();
->>>>>>> origin/release-v0.12
+
 
   std::vector<uint64_t> fees;
   for (uint32_t priority = 1; priority <= 4; ++priority)
