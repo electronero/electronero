12af6bd14b788404110e88e86bbf08d68ec7c7a9
Interchained <president@worldvaporexpo.com>
2018-06-26 11:12:36 -0400

    Update blockchain.h

--- src/cryptonote_core/blockchain.h
+++ src/cryptonote_core/blockchain.h
@@ -1007,6 +1007,10 @@ namespace cryptonote
     std::vector<uint64_t> m_timestamps;
     std::vector<difficulty_type> m_difficulties;
     uint64_t m_timestamps_and_difficulties_height;
+    
+    epee::critical_section m_difficulty_lock;
+    crypto::hash m_difficulty_for_next_block_top_hash;
+    difficulty_type m_difficulty_for_next_block;
 
     boost::asio::io_service m_async_service;
     boost::thread_group m_async_pool;
