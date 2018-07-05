039fd8f0a5691bfbeff48dcc31b9f33463d744e0
Interchained <president@worldvaporexpo.com>
2018-07-05 09:06:48 -0400

    Update blockchain.cpp

--- src/cryptonote_core/blockchain.cpp
+++ src/cryptonote_core/blockchain.cpp
@@ -861,6 +861,10 @@ difficulty_type Blockchain::get_difficulty_for_next_block()
   }
   if(HARD_FORK_SPLIT == 1)
   {
+  if ((uint64_t)height >= MAINNET_HARDFORK_V12_HEIGHT - 1 && (uint64_t)height <= MAINNET_HARDFORK_V12_HEIGHT + (uint64_t)DIFFICULTY_BLOCKS_COUNT_V12)
+  {
+  return (difficulty_type) 72289156;
+  }	  
   if ((uint64_t)height >= MAINNET_HARDFORK_V13_HEIGHT - 1 && (uint64_t)height <= MAINNET_HARDFORK_V13_HEIGHT + (uint64_t)DIFFICULTY_BLOCKS_COUNT_V12)
   {
   return (difficulty_type) 72289156;
