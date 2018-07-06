9fb2c97818bab0d3a35ce1c3de8c608a3cd70198
Interchained <president@worldvaporexpo.com>
2018-07-06 06:08:14 -0400

    FAKECHAIN

--- src/cryptonote_core/cryptonote_core.cpp
+++ src/cryptonote_core/cryptonote_core.cpp
@@ -376,7 +376,7 @@ namespace cryptonote
 
     if (test_options != NULL)
     {
-      m_nettype = MAINNET;
+      m_nettype = FAKECHAIN;
     }
     bool r = handle_command_line(vm);
     std::string m_config_folder_mempool = m_config_folder;
