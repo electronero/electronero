0a6967296023b557baa20c79ae6337cf6e7abc26
Interchained <president@worldvaporexpo.com>
2018-07-10 06:33:44 -0400

    Update object_sizes.cpp

--- src/debug_utilities/object_sizes.cpp
+++ src/debug_utilities/object_sizes.cpp
@@ -1,5 +1,6 @@
 // Copyright (c) 2017-2018, The Monero Project
 //
+master branch
 // All rights reserved.
 //
 // Redistribution and use in source and binary forms, with or without modification, are
