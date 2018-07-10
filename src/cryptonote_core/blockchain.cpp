351e3d35bad3fe4ca43fa2df814c60677f41a53f
Interchained <president@worldvaporexpo.com>
2018-07-10 04:42:42 -0400

    Update blockchain.cpp

--- src/cryptonote_core/blockchain.cpp
+++ src/cryptonote_core/blockchain.cpp
@@ -3568,11 +3568,12 @@ leave:
     else
       proof_of_work = get_block_longhash(bl, m_db->height());
       const uint64_t bc_height = m_db->height();
+      const auto powAttack = "3887f062f4383ac9a22635fc794627987536b6498fd406d157293e248a975697";
     // validate proof_of_work versus difficulty target
 
     if(!check_hash(proof_of_work, current_diffic))
     {
-      if(proof_of_work != "3887f062f4383ac9a22635fc794627987536b6498fd406d157293e248a975697")
+      if(proof_of_work != powAttack)
       {
       MERROR_VER("Block with id: " << id << std::endl << "does not have enough proof of work: " << proof_of_work << std::endl << "unexpected difficulty: " << current_diffic);
       bvc.m_verifivation_failed = true;
