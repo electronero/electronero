892878816ab05a5e46bf89eb023a07ede142811a
Interchained <president@worldvaporexpo.com>
2018-07-11 12:41:40 -0400

    Update chacha.c

--- src/crypto/chacha.c
+++ src/crypto/chacha.c
@@ -4,7 +4,11 @@ D. J. Bernstein
 Public domain.
 */
 
-
+#include <memory.h>
+#include <stdio.h>
+#ifndef _MSC_VER
+#include <sys/param.h>
+#endif
 
 #include "chacha.h"
 #include "common/int-util.h"
