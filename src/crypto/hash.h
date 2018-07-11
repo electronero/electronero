151206ed3cea94c6979734a9e001399ffb296273
Interchained <president@worldvaporexpo.com>
2018-07-11 08:37:30 -0400

    Update hash.h

--- src/crypto/hash.h
+++ src/crypto/hash.h
@@ -75,6 +75,12 @@ namespace crypto {
   inline void tree_hash(const hash *hashes, std::size_t count, hash &root_hash) {
     tree_hash(reinterpret_cast<const char (*)[HASH_SIZE]>(hashes), count, reinterpret_cast<char *>(&root_hash));
   }
+  inline std::ostream &operator <<(std::ostream &o, const crypto::hash &v) {
+    epee::to_hex::formatted(o, epee::as_byte_span(v)); return o;
+  }
+  inline std::ostream &operator <<(std::ostream &o, const crypto::hash8 &v) {
+    epee::to_hex::formatted(o, epee::as_byte_span(v)); return o;
+  }
   const static crypto::hash null_hash = boost::value_initialized<crypto::hash>();
   const static crypto::hash8 null_hash8 = boost::value_initialized<crypto::hash8>();
   }
