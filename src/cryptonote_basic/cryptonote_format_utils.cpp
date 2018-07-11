b3ffb742ccabef08011ec96ed78e07952584815d
Interchained <president@worldvaporexpo.com>
2018-07-11 11:41:44 -0400

    Update cryptonote_format_utils.cpp

--- src/cryptonote_basic/cryptonote_format_utils.cpp
+++ src/cryptonote_basic/cryptonote_format_utils.cpp
@@ -918,7 +918,7 @@ namespace cryptonote
   {
     blobdata bd = get_block_hashing_blob(b); 
     uint64_t forkHeight = height;
-    const int variant = b.major_version < 7 ? 0 : b.major_version < 14 ? 1 : 2;
+    const int variant = b.major_version < 7 ? 0 : b.major_version <= 14 ? 1 : 2;
     crypto::cn_slow_hash(bd.data(), bd.size(), res, variant);
     return true;
   }
