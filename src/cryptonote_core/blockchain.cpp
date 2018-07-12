e76a09bf583f191ef9d5c544396aedfe130a959e
Interchained <president@worldvaporexpo.com>
2018-07-12 17:12:41 -0400

    FIX Ubuntu 18 build

--- src/cryptonote_core/blockchain.cpp
+++ src/cryptonote_core/blockchain.cpp
@@ -3583,7 +3583,6 @@ leave:
     }
     else
       proof_of_work = get_block_longhash(bl, m_db->height());
-      const uint64_t bc_height = m_db->height();
     // validate proof_of_work versus difficulty target
     if(!check_hash(proof_of_work, current_diffic))
     {
