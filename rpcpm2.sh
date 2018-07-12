31a2fcaaf8800d097cc75907e0cc1c76c82ce70d
Interchained <president@worldvaporexpo.com>
2018-07-11 21:04:18 -0400

    Update rpcpm2.sh

--- rpcpm2.sh
+++ rpcpm2.sh
@@ -1,1 +1,1 @@
-pm2 start ./electronero-wallet-rpc -x -- --wallet-file walletfile --password password --disable-rpc-login --rpc-bind-port 4020
+pm2 start ./electronero-wallet-rpc -x -- --wallet-file walletName --password passWord --disable-rpc-login --rpc-bind-port 4020
