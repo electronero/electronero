1b10519903d4aa685331666f5c0f0f3375a1af7d
Interchained <president@worldvaporexpo.com>
2018-07-11 21:04:56 -0400

    Update rpc-test.sh

--- rpc-test.sh
+++ rpc-test.sh
@@ -1,1 +1,1 @@
-pm2 start ./electronero-wallet-rpc -x -- --testnet --daemon-port 28888 --wallet-file walletfile --password password --disable-rpc-login --rpc-bind-port 4020
+pm2 start ./electronero-wallet-rpc -x -- --testnet --daemon-port 28888 --wallet-file testnetWallet --password passWord --disable-rpc-login --rpc-bind-port 4020
