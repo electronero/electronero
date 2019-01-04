c77003bb2a18a65892bc31c903729979ef746a5d
Interchained <president@worldvaporexpo.com>
2019-01-04 17:16:31 -0500

    add libpcsclite-dev and remove libboost-all-dev
    Install Boost 1.64  and Open SSL 1.0. We'll review electronerod compile time on bare metal ubuntu installations and update install scripts accordingly as we progress.

--- ubuntu_install.sh
+++ ubuntu_install.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-apt-get install build-essential cmake pkg-config libboost-all-dev libzmq3-dev libssl-dev libsodium-dev libunbound-dev libminiupnpc-dev libunwind8-dev liblzma-dev libreadline6-dev libldns-dev libexpat1-dev libgtest-dev doxygen graphviz
+apt-get install build-essential cmake pkg-config libzmq3-dev libssl-dev libsodium-dev libunbound-dev libminiupnpc-dev libunwind8-dev libpcsclite-dev liblzma-dev libreadline6-dev libldns-dev libexpat1-dev libgtest-dev doxygen graphviz
 echo "Dependencies installation complete"
 echo "Download Submodules"
 
