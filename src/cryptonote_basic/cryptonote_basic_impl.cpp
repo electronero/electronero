568951d9cbc5a8112735b20047addd89c1362252
Interchained <president@worldvaporexpo.com>
2019-05-14 17:12:56 -0400

    adjust v20 to block 756471

--- src/cryptonote_basic/cryptonote_basic_impl.cpp
+++ src/cryptonote_basic/cryptonote_basic_impl.cpp
@@ -62,7 +62,7 @@ using namespace epee;
 #define MAINNET_HARDFORK_V17_HEIGHT ((uint64_t)(570000)) // MAINNET v17 hard fork
 #define MAINNET_HARDFORK_V18_HEIGHT ((uint64_t)(659000)) // MAINNET v18 hard fork
 #define MAINNET_HARDFORK_V19_HEIGHT ((uint64_t)(739800)) // MAINNET v19 hard fork
-#define MAINNET_HARDFORK_V20_HEIGHT ((uint64_t)(749660)) // MAINNET v20 hard fork
+#define MAINNET_HARDFORK_V20_HEIGHT ((uint64_t)(756471)) // MAINNET v20 hard fork
 
 #define TESTNET_ELECTRONERO_HARDFORK ((uint64_t)(12746)) // Electronero TESTNET fork height
 #define TESTNET_HARDFORK_V1_HEIGHT ((uint64_t)(1)) // TESTNET v1 
@@ -165,8 +165,8 @@ namespace cryptonote {
       reward = electronero_genesis;
       return true;
     }
-    const uint64_t goldnero_genesis = 9000000103090031291U;
-    if ((uint64_t)height == 749680) {
+    const uint64_t goldnero_genesis = 7400000103090031291U;
+    if ((uint64_t)height == 756475) {
       reward = goldnero_genesis;
       return true;
     }
@@ -199,7 +199,7 @@ namespace cryptonote {
     }
     
    // maybe work on better final subsidy later - todo, once emissions is final 
-   const uint64_t FINAL_SUBSIDY_ACTIVATOR = 66600U;
+   const uint64_t FINAL_SUBSIDY_ACTIVATOR = 66600000000U;
     if (base_reward < FINAL_SUBSIDY_ACTIVATOR){
      if (already_generated_coins >= TOKEN_SUPPLY){
        base_reward = FINAL_SUBSIDY_PER_MINUTE;
