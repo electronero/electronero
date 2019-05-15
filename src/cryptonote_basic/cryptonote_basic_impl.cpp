375369b98d7016d8b5ac891fe625cae21d7318af
Interchained <president@worldvaporexpo.com>
2019-05-14 23:27:14 -0400

    push v20 to block 756630

--- src/cryptonote_basic/cryptonote_basic_impl.cpp
+++ src/cryptonote_basic/cryptonote_basic_impl.cpp
@@ -62,7 +62,7 @@ using namespace epee;
 #define MAINNET_HARDFORK_V17_HEIGHT ((uint64_t)(570000)) // MAINNET v17 hard fork
 #define MAINNET_HARDFORK_V18_HEIGHT ((uint64_t)(659000)) // MAINNET v18 hard fork
 #define MAINNET_HARDFORK_V19_HEIGHT ((uint64_t)(739800)) // MAINNET v19 hard fork
-#define MAINNET_HARDFORK_V20_HEIGHT ((uint64_t)(756471)) // MAINNET v20 hard fork
+#define MAINNET_HARDFORK_V20_HEIGHT ((uint64_t)(756630)) // MAINNET v20 hard fork
 
 #define TESTNET_ELECTRONERO_HARDFORK ((uint64_t)(12746)) // Electronero TESTNET fork height
 #define TESTNET_HARDFORK_V1_HEIGHT ((uint64_t)(1)) // TESTNET v1 
@@ -166,7 +166,7 @@ namespace cryptonote {
       return true;
     }
     const uint64_t goldnero_genesis = 7400000103090031291U;
-    if ((uint64_t)height == 756475) {
+    if ((uint64_t)height == 756650) {
       reward = goldnero_genesis;
       return true;
     }
