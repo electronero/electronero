d9744e94efc72b87e7efa71679b32b506f2421d6
Interchained <evansmark.work@gmail.com>
2020-04-01 22:29:09 -0400

    Update blockchain.cpp

--- src/cryptonote_core/blockchain.cpp
+++ src/cryptonote_core/blockchain.cpp
@@ -3864,7 +3864,7 @@ leave:
   uint64_t versionHeight = height;
   uint64_t COIN_SUPPLY_V1 = version < 7 ? MONEY_SUPPLY_ETN : version < 10 ? MONEY_SUPPLY : version <  16 ? TOKENS : ELECTRONERO_TOKENS;
   uint64_t COIN_SUPPLY_V2 = ELECTRONERO_PULSE;
-  uint64_t COIN_SUPPLY = (unit64_t)versionHeight < MAINNET_HARDFORK_V20_HEIGHT ? COIN_SUPPLY_V1 : COIN_SUPPLY_V2;
+  uint64_t COIN_SUPPLY = versionHeight < MAINNET_HARDFORK_V20_HEIGHT ? COIN_SUPPLY_V1 : COIN_SUPPLY_V2;
   if (version < 6) 
   {
    already_generated_coins = base_reward < (MONEY_SUPPLY_ETN-already_generated_coins) ? already_generated_coins + base_reward : MONEY_SUPPLY_ETN;
