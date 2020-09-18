28efd3a3a3d1637ad07f287f60eccf8fdd86361a
Interchained <evansmark.work@gmail.com>
2020-09-18 01:51:15 -0400

    fix v7 height

--- src/cryptonote_core/blockchain.cpp
+++ src/cryptonote_core/blockchain.cpp
@@ -62,7 +62,7 @@
 #define MONERO_DEFAULT_LOG_CATEGORY "blockchain"
 
 #define MAINNET_HARDFORK_V1_HEIGHT ((uint64_t)(1)) // MAINNET v1 
-#define MAINNET_HARDFORK_V7_HEIGHT ((uint64_t)(307001)) // MAINNET v7 hard fork 
+#define MAINNET_HARDFORK_V7_HEIGHT ((uint64_t)(306943)) // MAINNET v7 hard fork 
 #define MAINNET_HARDFORK_CRYSTALEUM_HEIGHT ((uint64_t)(307004)) // MAINNET crystaleum hard fork 
 
 #define TESTNET_HARDFORK_V1_HEIGHT ((uint64_t)(1)) // TESTNET v1 
