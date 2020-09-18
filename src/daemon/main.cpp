f6b19bce14b9ed24d77b96e5003ce7f38ccfdc22
Interchained <evansmark.work@gmail.com>
2020-09-18 03:09:16 -0400

    Update main.cpp

--- src/daemon/main.cpp
+++ src/daemon/main.cpp
@@ -276,7 +276,7 @@ int main(int argc, char const * argv[])
       tools::set_max_concurrency(command_line::get_arg(vm, daemon_args::arg_max_concurrency));
 
     // logging is now set up
-    MGINFO("Electronero '" << MONERO_RELEASE_NAME << "' (v" << MONERO_VERSION_FULL << ")");
+    MGINFO("Crystaleum '" << MONERO_RELEASE_NAME << "' (v" << MONERO_VERSION_FULL << ")");
 
     MINFO("Moving from main() into the daemonize now.");
 
