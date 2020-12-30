e365d75e60611315dfdc647ad4871ceb64e58067
Interchained <evansmark.work@gmail.com>
2020-12-30 00:43:34 -0500

    Update compile_electronero_network_nodes.sh

--- compile_electronero_network_nodes.sh
+++ compile_electronero_network_nodes.sh
@@ -1,23 +1,11 @@
-git submodule init
-git submodule update
-cd coins/electronero
-git submodule init
-git submodule update
-make -j4
-cd ../electroneropulse
-git submodule init
-git submodule update
-make -j4
-cd ../litenero
-git submodule init
-git submodule update
-make -j4
-cd ../goldnero
-git submodule init
-git submodule update
-make -j4
-cd coins/crystaleum
-git submodule init
-git submodule update
-make -j4
+git submodule init && git submodule update && cd coins/electronero
+git submodule init && git submodule update
+make -j4 && cd ../electroneropulse
+git submodule init && git submodule update
+make -j4 && cd ../litenero
+git submodule init && git submodule update
+make -j4 && cd ../goldnero
+git submodule init && git submodule update
+make -j4 && cd coins/crystaleum
+git submodule init && git submodule update && make -j4
 echo 'Electronero network compiled ETNX, ETNXP, LTNX, GLDX, CRFI successfully'
