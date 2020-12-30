fd46700c49d31c58fb1394ab93dbf150edd4642a
Interchained <evansmark.work@gmail.com>
2020-12-29 23:14:26 -0500

    fix syntax

--- compile_electronero_network_nodes.sh
+++ compile_electronero_network_nodes.sh
@@ -20,4 +20,4 @@ cd coins/crystaleum
 git submodule init
 git submodule update
 make -j4
-echo 'Electronero network compiled ETNX, ETNXP, LTNX, GLDX, CRFI successfully" 
+echo 'Electronero network compiled ETNX, ETNXP, LTNX, GLDX, CRFI successfully'
